587bcbcf1113e04d965351de33d56442be32dccd
ReitumetseM <pheladim11@gmail.com>
2023-07-18 06:04:09 -0700

    Nested loops

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * adds two integers and returns the result.
+ * Ga: int to be added to b
+ * Gb: int to be added to a
+ * Return: sum of a and b
+ */
+int add(int, int)
+{
+	int c;
+	c = a + b;
+	return (c);
+}
