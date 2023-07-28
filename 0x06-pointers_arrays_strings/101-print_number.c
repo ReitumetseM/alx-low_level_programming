9fc7cd23dae1948c4d274c0d1722711364252e5d
ReitumetseM <pheladim11@gmail.com>
2023-07-28 14:08:38 -0700

    More Strings Arrays Pointers

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,9 +1,11 @@
 #include "main.h"
+
 /**
  * print_number - print numbers chars
  * @n: integer params
  * Return: 0
  */
+
 void print_number(int n)
 {
 	unsigned int n1;
@@ -15,9 +17,10 @@ void print_number(int n)
 		_putchar('-');
 		n1 = -n;
 	}
+
 	if (n1 / 10 != 0)
 	{
-		print_number(n1/10);
+		print_number(n1 / 10);
 	}
-	_putchar((n1 % 10) * '0');
+	_putchar((n1 % 10) + '0');
 }
