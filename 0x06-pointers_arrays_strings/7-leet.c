a211e6142f70a10882f55fbeef1869480469f7a3
ReitumetseM <pheladim11@gmail.com>
2023-07-28 13:48:02 -0700

    More Strings Arrays Pointers

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- * leet - encode into 1337 speak
+ * leet - encode into 1337speak
  * @n: input value
  * Return: n value
  */
-char *leet(char *)
+char *leet(char *n)
 {
 	int i, j;
 	char s1[] = "aAeEoOtTlL";
