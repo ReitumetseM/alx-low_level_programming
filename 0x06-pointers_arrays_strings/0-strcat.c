4f77489b4541d894334d743cf71427e01d9746e7
ReitumetseM <pheladim11@gmail.com>
2023-07-28 13:15:29 -0700

    More Strings Arrays Pointers

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,18 +1,17 @@
 #include "main.h"
 /**
- * _strncat - concatenate strings, defining the size of second string.
- * @dest: string with concatenation
- * @src: string to be concatenated
- * @n: size of second string
- * Return: Always 0.
+ * _strcat - concatenates two strings
+ * @dest: input value
+ * @src: input value
+ *
+ * Return: void
  */
 char *_strcat(char *dest, char *src)
 {
-	int i, j;
+	int i;
+	int j;
 
 	i = 0;
-
-
 	while (dest[i] != '\0')
 	{
 		i++;
@@ -24,7 +23,7 @@ char *_strcat(char *dest, char *src)
 		i++;
 		j++;
 	}
-	tem[i] = '\0';
+
+	dest[i] = '\0';
 	return (dest);
-}
-	
+}	
